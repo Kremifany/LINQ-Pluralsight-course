08d4c421d337e6d9c4b1730281f07ec70b50342a
Kremifany <34751960+Kremifany@users.noreply.github.com>
2020-02-02 15:11:37 -0500

    First commit

---
+++ Cars/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+
+            var result = cars.All(c => c.Manufacturer == "BMW");
+            Console.WriteLine(result);
+            var query =
+                from car in cars
+                where car.Manufacturer == "BMW"
+                orderby car.Combined descending, car.Name ascending
+                select car;
+            var top = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                             .OrderByDescending(c => c.Combined)
+                             .ThenBy(c => c.Name)
+                             .Select(c => c)
+                             .First();
+            Console.WriteLine(top.Name);
+            //var query = cars.OrderByDescending(c => c.Combined)
+            //                .ThenBy(c => c.Name);
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($":{car.Manufacturer}--{car.Year}-{car.Name} : {car.Combined}");
+            }
+            //foreach (var car in cars)
+            //{
+            //    console.writeline(car.name);
+            //}
+        }
+        private static List<Car> ProcessFile(string path)
+        {
+            //return
+            //    File.ReadAllLines(path)
+            //        .Skip(1)
+            //        .Where(line => line.Length > 1)
+            //        .Select(Car.ParseFromCsv)
+            //        .ToList();
+            var query =
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(l => l.Length > 1)
+                    .ToCar();
+
+            return query.ToList();
+        }
+    }
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach(var line in source)
+            {
+                var columns = line.Split(',');
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cilinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+        }
+        
+    }
+
+}
---
+++ Features/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        { 
+
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(3));
+            Console.WriteLine("+++++++++");
+
+            Func<int, int, int> add = (x, y) => x + y;
+            Console.WriteLine(square(add(3,5)));
+
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name= "Scott" },
+                new Employee {Id = 2, Name = "Chris" },
+                new Employee {Id = 3, Name = "Alexa"}
+            };
+            Console.WriteLine("+++++++++");
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee {Id = 3, Name = "Alex"}
+            };
+
+            Console.WriteLine(developers.Count());
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+
+            Console.WriteLine("+++++++++");
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
+            Console.WriteLine("+++++++++");
+            foreach (var employee in developers.Where(
+                e => e.Name.StartsWith("C")))
+            {
+                Console.WriteLine(employee.Name);
+            }
+            Console.WriteLine("+++++++++");
+            
+            //method syntax
+            var query1 = developers.Where(e => e.Name.Length == 5)
+                                   .OrderByDescending(e => e.Name)
+                                   .Select(e => e);
+
+            Console.WriteLine("+++++++++");
+
+            //query syntax
+            var query2 = from developer in developers
+                         where developer.Name.Length == 5
+                         orderby developer.Name descending
+                         select developer;                         ;
+
+            foreach (var employee in query1)
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }
+    }
+}
