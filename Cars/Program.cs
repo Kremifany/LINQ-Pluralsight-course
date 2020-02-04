0054f4a27cc372c1ecb0c564734e9344d3b485ba
Kremifany <34751960+Kremifany@users.noreply.github.com>
2020-02-03 23:42:22 -0500

    Agregating data query added

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -15,48 +15,27 @@ namespace Cars
             var manufacturers = ProcessManufacturers("manufacturers.csv");
 
             var query =
-                from manufacturer in manufacturers
-                join car in cars on manufacturer.Name equals car.Manufacturer
-                    into carGroupByManufacturerName
-                orderby manufacturer.Name
+                from car in cars
+                group car by car.Manufacturer into carGroup
                 select new
                 {
-                    Manufacturer = manufacturer,
-                    Cars = carGroupByManufacturerName
-                };
-
-            var query3 =
-                from manufacturer in manufacturers
-                join car in cars on manufacturer.Name equals car.Manufacturer
-                    into carGroup
-                orderby manufacturer.Headquarters
-                select new
-                {
-                    Manufacturer = manufacturer,
-                    Cars = carGroup
+                    Name = carGroup.Key,
+                    Max = carGroup.Max(c => c.Combined),
+                    Min = carGroup.Min(c => c.Combined),
+                    Avg = carGroup.Average(c => c.Combined)
                 } into result
-                group result by result.Manufacturer.Headquarters;
-
-
-
-
-            var query2 =
-                manufacturers.GroupJoin(cars, m => m.Name, c => c.Manufacturer, (m, g) =>
-                 new
-                 {
-                     Manufacturer = m,
-                     Cars = g
-                 }).OrderBy(m => m.Manufacturer.Name);
-
-            foreach (var group in query3)
+                orderby result.Max descending
+                select result;
+                
+            foreach (var result in query)
             {
-                Console.WriteLine($"{group.Key}");
-                foreach (var car in group.SelectMany(g =>g.Cars).OrderByDescending(c => c.Combined).Take(3))
-                {
-                    Console.WriteLine($"\t{car.Name} : {car.Combined}");
-                }
+                Console.WriteLine($"{result.Name}");
+                Console.WriteLine($"\tMax : {result.Max}");
+                Console.WriteLine($"\tMin : {result.Min}");
+                Console.WriteLine($"\tAverage : {result.Avg}");
+
             }
-           
+
         }
         private static List<Car> ProcessCars(string path)
         {
